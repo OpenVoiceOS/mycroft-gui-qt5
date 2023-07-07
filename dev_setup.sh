02389986c5fad657b902ee1d576b72ef21a29384
JarbasAi <jarbasai@mailfence.com>
2023-07-07 15:49:56 +0100

    fix cmake for qt5

--- dev_setup.sh
+++ dev_setup.sh
@@ -139,7 +139,7 @@ function build_gui() {
     mkdir build-testing
     fi
     cd build-testing
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release   -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+    cmake .. -DBUILD_WITH_QT6=OFF -DQT_MAJOR_VERSION=5 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
     make -j4
     sudo make install
     install_lottie
