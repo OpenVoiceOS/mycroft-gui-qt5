d1b8a8ded0ba1f3ed2faa8c110b1ed5128434e14
JarbasAi <jarbasai@mailfence.com>
2024-02-28 23:42:51 +0000

    fix/renamed packages for qt5

--- dev_setup.sh
+++ dev_setup.sh
@@ -69,10 +69,10 @@ kubuntu() {
 manjaro() {
     echo "Starting Installation For Manjaro / Arch"
     echo ""
-    echo "Following Packages Will Be Installed: cmake extra-cmake-modules kio kio-extras plasma-framework qt5-websockets qt5-webview qt5-declarative qt5-multimedia qt5-quickcontrols2 qt5-webengine qt5-base"
+    echo "Following Packages Will Be Installed: cmake extra-cmake-modules kio5 kio-extras plasma-framework5 qt5-websockets qt5-webview qt5-declarative qt5-multimedia qt5-quickcontrols2 qt5-webengine qt5-base"
     echo ""
     echo "Please Enter Authentication For Installing System Dependencies"
-    sudo pacman -S git cmake extra-cmake-modules kio kio-extras plasma-framework qt5-websockets qt5-webview qt5-declarative qt5-multimedia qt5-quickcontrols2 qt5-webengine qt5-base
+    sudo pacman -S git cmake extra-cmake-modules kio5 kio-extras plasma-framework5 qt5-websockets qt5-webview qt5-declarative qt5-multimedia qt5-quickcontrols2 qt5-webengine qt5-base
     build_gui   
 }
 
